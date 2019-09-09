4512bd5af477f2001c696a02631ed37fe4a0e0a5
msraya <msraya@gmail.com>
2019-09-09 18:21:05 +0200

    Remove files from src and solve icons problems

--- src/compile.bat
+++
@@ -1,4 +0,0 @@
-rem del /S /Q dist
-rem del /S /Q build
-rem c:\python37\scripts\pyinstaller -wF --onefile --windowed --icon=./main.ico kicadpartman.py
-c:\python37\scripts\pyinstaller kicadpartman.spec
